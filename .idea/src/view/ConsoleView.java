dd43084285f292e16db86b8e49b4f823afc3610a
chelsyilovia <chelsyiloviarosali@webmail.umm.ac.id>
2026-07-02 16:22:42 +0700

    feat : menambahkan import yang diperlukan

--- .idea/src/view/ConsoleView.java
+++ .idea/src/view/ConsoleView.java
@@ -1,4 +1,9 @@
 package view;
+import model.Putusan;
+import model.StatistikPutusan;
+import util.InputHandler;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 public class ConsoleView {
 }
