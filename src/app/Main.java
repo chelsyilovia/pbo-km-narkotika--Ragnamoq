299dbbaf69e9a87aad8a67f433b4883ab5450515
GHAZI0606 <alghifariazizi@webmail.umm.ac.id>
2026-07-02 15:11:52 +0700

    feat: menambahkan alur utama aplikasi

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,32 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import controller.KnowledgeController;
+import model.KnowledgeRepository;
+import model.Putusan;
+import model.StatistikPutusan;
+import util.DataGenerator;
+import view.ConsoleView;
+
 public class Main {
-}
+
+    public static void main(String[] args) {
+        KnowledgeRepository repository = new KnowledgeRepository();
+
+        ArrayList<Putusan> dataSampel = DataGenerator.generateSampleData();
+        for (Putusan p : dataSampel) {
+            repository.simpan(p);
+        }
+
+        KnowledgeController controller = new KnowledgeController(repository);
+        ConsoleView view = new ConsoleView();
+        Scanner scanner = view.getScanner();
+
+        view.tampilkanPesan("Selamat datang di KMS Putusan Pengadilan Narkotika.");
+        view.tampilkanPesan(dataSampel.size() + " data sampel berhasil dimuat. Total objek Putusan dibuat: "
+                + Putusan.getJumlahDibuat());
+
+        boolean berjalan = true;
+        while (berjalan) {
+            int pilihan = view.tampilkanMenu();
